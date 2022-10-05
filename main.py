87a67bde6c340d516245c080f008ea8ff0a697d2
rain <yunwei.zh@gmail.com>
2022-10-05 19:43:34 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import time
+import tensorflow as tf
+import os
+import numpy as np
+import json
+
+with open('herolist.json', 'r', encoding='utf-8') as f:
+    hero_data = json.load(f)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+dim = 123
+
+saved_model = tf.keras.models.load_model('saved_model/2.h5')
+
+testPool = [0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0,
+            0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+            1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+            ]
+
+print("英雄池：")
+for hero_index, is_exist in enumerate(testPool):
+    if is_exist == 1:
+        print(hero_data[str(hero_index + 1)], end=",")
+print("\n")
+
+test_in = []
+hero_names = []
+for hero_index, is_exist in enumerate(testPool):
+    if is_exist == 1:
+        hero_select = np.zeros(dim)
+        hero_select[hero_index] = 1
+        hero_names.append(hero_data[str(hero_index + 1)])
+        test_in.append([testPool, hero_select])
+
+test_in = np.array(test_in)
+
+clock = time.time()
+#out_list = saved_model.predict(test_in)
+out_list = saved_model(test_in, training=False)
+
+print(time.time() - clock, 's')
+result_list = []
+for hero_index, item in enumerate(out_list):
+    result_list.append([hero_names[hero_index], float(item[0])])
+
+
+def takeSecond(elem):
+    return elem[1]
+
+
+result_list.sort(key=takeSecond, reverse=True)
+
+print("推荐选择：")
+recommend_count = 0
+for item in result_list:
+    print(item)
+    recommend_count = recommend_count + 1
+    if recommend_count > 10:
+        break
---
+++ train.py
@@ -0,0 +1,58 @@
+import numpy
+import os
+import numpy as np
+import json
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Activation
+
+with open('herolist.json', 'r', encoding='utf-8') as f:
+    hero_data = json.load(f)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+dim = 123
+
+hero_pool = np.genfromtxt("heropool.csv", skip_header=1, delimiter=',', dtype=int)
+hero_wins = np.genfromtxt("herowins.csv", skip_header=1, delimiter=',', dtype=int)
+data_length = hero_pool.shape[0]
+sample_in = []
+sample_out = []
+
+for data_index, line in enumerate(hero_pool):
+    for hero_index, is_exist in enumerate(hero_pool[data_index]):
+        if is_exist == 1:
+            hero_select = np.zeros(dim)
+            hero_select[hero_index] = 1
+            sample_in.append([line, hero_select])
+            if hero_wins[data_index, hero_index] == 1:
+                sample_out.append(1.0)
+            else:
+                sample_out.append(0.0)
+
+sample_in = numpy.array(sample_in)
+sample_out = numpy.array(sample_out)
+
+x_train, x_test, y_train, y_test = train_test_split(sample_in, sample_out, test_size=0.1, shuffle=True)
+
+model = Sequential()
+model.add(LSTM(256, input_shape=(2, dim), return_sequences=False))
+model.add(Dropout(0.1))
+model.add(Dense(10, activation="relu"))
+model.add(Dropout(0.1))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+print("fit:")
+model.fit(x_train, y_train, epochs=5, batch_size=512)
+
+print("eval:")
+mae = model.evaluate(x_test, y_test, verbose=2)
+print(mae)
+
+model.save('saved_model/2.h5', save_format='h5')
