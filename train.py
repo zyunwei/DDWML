7b714a0acfa4f6eb51a0fc36004c01324fe6738f
rain <yunwei.zh@gmail.com>
2022-11-01 17:02:32 +0800

    20221001_20221101

--- train.py
+++ train.py
@@ -54,4 +54,4 @@ print("eval:")
 mae = model.evaluate(x_test, y_test, verbose=2)
 print(mae)
 
-model.save('saved_model/20220907_20221007.h5', save_format='h5')
+model.save('saved_model/20221001_20221101.h5', save_format='h5')
