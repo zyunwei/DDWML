9e795eae560318b1ac2fd32f2d27c57fc7ea37c1
yunwei.zh@gmail.com <yunwei.zh@gmail.com>
2025-04-01 15:34:50 +0800

    20250401

--- train.py
+++ train.py
@@ -54,4 +54,4 @@ print("eval:")
 mae = model.evaluate(x_test, y_test, verbose=2)
 print(mae)
 
-model.save('saved_model/20250201_20250301.h5', save_format='h5')
+model.save('saved_model/20250301_20250401.h5', save_format='h5')
