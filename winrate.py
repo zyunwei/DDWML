b40fdf892de020f7b5c0a65fbfbef08b0de6eec5
yunwei.zh@gmail.com <yunwei.zh@gmail.com>
2025-07-27 15:33:30 +0800

    20250801

--- winrate.py
+++ winrate.py
@@ -11,7 +11,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 dim = 126
 
-saved_model = tf.keras.models.load_model('saved_model/20250601_20250701.h5')
+saved_model = tf.keras.models.load_model('saved_model/20250701_20250801.h5')
 
 heroScoreList = []
 heroTimesList = []
